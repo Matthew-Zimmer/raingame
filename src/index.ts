bde2c5a315a7c763858369439a597725e82576ab
Matthew Zimmer <the.matthew.zimmer@gmail.com>
2021-04-16 15:52:07 -0400

    added hello world text

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
 
 function main() {
-    console.log('Canvas loaded');
+    const ctx = (document.getElementById('canvas') as HTMLCanvasElement).getContext('2d')!;
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, 10000, 1000);
+    ctx.fill();
+    
+    ctx.font = '36px serif';
+    ctx.fillStyle = 'white';
+    ctx.fillText('Hello world', 20, 50);
 }
