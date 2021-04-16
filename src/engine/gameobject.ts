5cac02287f9e886cd8c35273fefe6b5af2a4fa7d
Matthew Zimmer <the.matthew.zimmer@gmail.com>
2021-04-16 18:31:08 -0400

    added event system for mouse clicks

--- src/engine/gameobject.ts
+++ src/engine/gameobject.ts
@@ -6,13 +6,18 @@ let gameobject_count = 0;
 export abstract class gameobject {
     public collider: collider;
 
-    constructor(pt: point, size: size, public id: number = gameobject_count++) {
+    constructor(pt: point, size: size, private id: number = gameobject_count++) {
         this.collider = new collider(pt, size);
     }
 
     draw(ctx: CanvasRenderingContext2D) { }
     update(dt: number) {}
     collided_with(other: gameobject) {}
+    clicked(pt: point) {}
     start() {}
     end() {}
+
+    eq(other: gameobject) {
+        return this.id == other.id;
+    }
 }
