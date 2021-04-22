6b76e7f2b5e0270d9036725e6ddaa61dc0a7de51
Matthew Zimmer <the.matthew.zimmer@gmail.com>
2021-04-21 23:19:32 -0400

    added praying

--- src/engine/global_events.ts
+++ src/engine/global_events.ts
@@ -4,4 +4,5 @@ import { point } from './metric.js';
 export const global_events = {
     on_plant_matured: event<(pt: point) => Promise<void>>(),
     on_tree_matured: event<(pt: point) => Promise<void>>(),
+    on_prayed: event<(piety: number) => void>(),
 };
