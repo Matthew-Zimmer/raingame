5d443e6af9ff815c3894277598ff39475ccca691
Andrew Taylor <andrew.taylor.4378@gmail.com>
2021-04-22 03:18:29 +0000

    player feature

---
+++ src/base/component/spell.ts
@@ -0,0 +1,12 @@
+
+export type spell_kind = 'rain' | 'lightning';
+
+export class spell {
+    constructor(public name: spell_kind, public mana_cost: number) {
+    }
+}
+
+export const spell_book= {
+    rain: new spell("rain", 0),
+    lightning: new spell("lightning", 3) 
+};
--- src/base/feature/player.ts
+++ src/base/feature/player.ts
@@ -1,4 +1,43 @@
-export class player {
+import { feature } from "../../engine/feature";
+import { spell, spell_book, spell_kind } from "../component/spell";
+import { rain } from "../gameobject/rain";
+
+
+
+
+export class player_feature extends feature({}) 
+{
+    active: number = 0;
+    spells: spell[] = [spell_book.rain];
+
     constructor(public mana: number){
+        super();
     }
+
+    // active_spell() {
+    //     return this.spells[this.active];
+    // }
+    gain_spell(kind: spell_kind, mana_cost: number){
+        this.spells.push(new spell(kind, mana_cost))
+    }
+    use_spell(spell: spell) {
+        this.use_mana(spell.mana_cost);
+    }
+
+    change_spell(kind: spell_kind){
+        for(let i = 0; i < this.spells.length; i++) {
+            if(this.spells[i].name === kind) {
+                this.active = i;
+            }
+        }
+        return this.active;
+    }
+
+    use_mana(mana: number){
+        this.mana -= mana;
+    }
+    gain_mana(mana: number){
+        this.mana += mana;
+    }
+
 }
