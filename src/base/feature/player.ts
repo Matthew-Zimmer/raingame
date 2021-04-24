a9f15833859b10d36ca2db833226753ce769d5e4
Matthew Zimmer <the.matthew.zimmer@gmail.com>
2021-04-24 14:45:43 -0400

    implemented player

--- src/base/feature/player.ts
+++ src/base/feature/player.ts
@@ -1,8 +1,14 @@
+import { event } from "../../engine/event";
 import { feature } from "../../engine/feature";
+import { point } from "../../engine/metric";
 import { spell, spell_book, spell_kind } from "../component/spell";
 import { rain } from "../gameobject/rain";
 
-export class player_feature extends feature({}) 
+export class player_feature extends feature({
+    on_spell_gained: event<(spell: spell) => void>(),
+    on_mana_change: event<(mana: number) => void>(),
+    activate_spell_at: event<(spell: spell, pt: point) => void>(),
+})
 {
     spells: spell[] = [spell_book.rain];
 
@@ -10,19 +16,32 @@ export class player_feature extends feature({})
         super();
     }
 
-    
-    gain_spell(kind: spell_kind, mana_cost: number){
-        this.spells.push(new spell(kind, mana_cost))
-    }
-    use_spell(spell: spell) {
-        this.use_mana(spell.mana_cost);
+    private gain_spell(spell: spell) {
+        this.spells.push(spell);
+        this.notify_all('on_spell_gained',cb => cb(spell));
     }
 
-    use_mana(mana: number){
-        this.mana -= mana;
-    }
-    gain_mana(mana: number){
-        this.mana += mana;
+    use_spell_at(spell: spell, pt: point) {
+        if (this.mana >= spell.mana_cost) {
+            this.use_mana(spell.mana_cost);
+            this.notify_all('activate_spell_at', cb => cb(spell, pt));
+        }
     }
 
+    private change_mana(mana: number) {
+        this.mana = mana;
+        this.notify_all('on_mana_change', cb => cb(this.mana));
+    }
+
+    use_mana(mana: number) {
+        this.change_mana(this.mana - mana);
+    }
+
+    gain_mana(mana: number) {
+        this.change_mana(this.mana + mana);
+    }
+
+    get_spell(kind: spell_kind) {
+        return this.spells.find(x => x.kind === kind);
+    }
 }
